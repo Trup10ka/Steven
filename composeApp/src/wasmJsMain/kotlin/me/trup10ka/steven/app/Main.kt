3ac3edb9a75ae8441f16a7b4da7cc169ee419e05
Trup10ka <lukys3036@gmail.com>
2024-03-28 16:38:05 +0100

    Update ID of html canvas

---
+++ composeApp/src/wasmJsMain/kotlin/me/trup10ka/steven/app/Main.kt
@@ -0,0 +1,10 @@
+package me.trup10ka.steven.app
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main()
+{
+    CanvasBasedWindow(canvasElementId = "steven-compose-canvas") { StevenApp() }
+}
