ef8123b4ec2d15806bc9d7fe39bbb570f73ca176
Trup10ka <lukys3036@gmail.com>
2024-04-17 19:21:18 +0200

    Add kotlinx coroutines for JS

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -19,10 +19,11 @@ kotlin {
         
         commonMain.dependencies {
             implementation(projects.shared)
+            implementation(libs.kotlinx.coroutines.core)
         }
 
         jsMain.dependencies {
-            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.kotlinx.coroutines.js)
             implementation(libs.kotlinx.serialization.json)
         }
     }
