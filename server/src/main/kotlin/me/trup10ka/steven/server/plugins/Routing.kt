e7bcf450bc5bdfc18c4b160ea775b08e8eb411c2
Trup10ka <lukys3036@gmail.com>
2024-04-19 12:10:02 +0200

    Move server constants to server module

--- server/src/main/kotlin/me/trup10ka/steven/server/plugins/Routing.kt
+++ server/src/main/kotlin/me/trup10ka/steven/server/plugins/Routing.kt
@@ -1,13 +1,13 @@
 package me.trup10ka.steven.server.plugins
 
-import me.trup10ka.shared.JS_DIR
-import me.trup10ka.shared.STYLES_DIR
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import me.trup10ka.steven.server.routing.areYouATeacher
 import me.trup10ka.steven.server.routing.lookForEvent
+import me.trup10ka.steven.server.util.JS_DIR
+import me.trup10ka.steven.server.util.STYLES_DIR
 import me.trup10ka.steven.server.util.throwIfFileDoesNotExist
 import java.io.File
 
