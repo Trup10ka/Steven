4a97fe2ba6a7e9429e054a863170f4465cdbf725
Trup10ka <lukys3036@gmail.com>
2024-04-19 22:10:34 +0200

    Add EventManager and TeacherVanguard initialization in stevenModule

--- server/src/main/kotlin/me/trup10ka/steven/server/StevenAppModules.kt
+++ server/src/main/kotlin/me/trup10ka/steven/server/StevenAppModules.kt
@@ -1,10 +1,14 @@
 package me.trup10ka.steven.server
 
 import io.ktor.server.application.*
+import me.trup10ka.steven.server.event.EventManager
+import me.trup10ka.steven.server.event.TeacherVanguard
 import me.trup10ka.steven.server.plugins.configureRouting
-
 
 fun Application.stevenModule()
 {
-    configureRouting()
+    val teacherVanguard = TeacherVanguard()
+    val eventManager = EventManager(teacherVanguard)
+
+    configureRouting(eventManager, teacherVanguard)
 }
