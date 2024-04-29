f5bf3bfe5d5e9b6c1ad3ca995deaa0380b231b6d
Trup10ka <lukys3036@gmail.com>
2024-04-29 16:21:22 +0200

    Add member id for StevenClient

--- webapp/src/jsMain/kotlin/me/trup10ka/steven/app/StevenClient.kt
+++ webapp/src/jsMain/kotlin/me/trup10ka/steven/app/StevenClient.kt
@@ -1,5 +1,8 @@
 package me.trup10ka.steven.app
 
+import kotlinx.browser.window
+import me.trup10ka.shared.util.IdType.MEMBER
+import me.trup10ka.shared.util.idOf
 import me.trup10ka.steven.app.pages.InvitePage
 import me.trup10ka.steven.app.pages.MapPage
 import me.trup10ka.steven.app.pages.Page
@@ -8,12 +11,15 @@ class StevenClient(currentRoute: String)
 {
     private lateinit var page: Page
 
+    val memberId: String
+        get() = getLastPathSegment() idOf MEMBER
+
     init
     {
         when (currentRoute)
         {
             "/" -> page = InvitePage()
-            "/take-me-in" -> page = MapPage()
+            "/take-me-in" -> page = MapPage(this)
 
             else -> console.error("Unknown route: $currentRoute, anything regarding script will not work")
         }
@@ -23,5 +29,10 @@ class StevenClient(currentRoute: String)
     {
         if (::page.isInitialized)
             page.setupPage()
+    }
+
+    private fun getLastPathSegment(): String
+    {
+        return window.location.pathname.split("/").last()
     }
 }
