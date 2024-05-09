054323043dab115626b526621c2662399c64755a
Trup10ka <lukys3036@gmail.com>
2024-05-09 22:57:04 +0200

    Implement temporary teacher vanguard bypass

--- server/src/main/kotlin/me/trup10ka/steven/server/event/TeacherVanguard.kt
+++ server/src/main/kotlin/me/trup10ka/steven/server/event/TeacherVanguard.kt
@@ -4,7 +4,14 @@ import me.trup10ka.shared.data.EventMember
 
 class TeacherVanguard
 {
+
     private val teachers = mutableListOf<EventMember>()
+
+    // TODO: REMOVE THIS
+    init
+    {
+        teachers.add(EventMember("12345", "1", "1", true, "1"))
+    }
 
     fun addTeacher(teacher: EventMember)
     {
