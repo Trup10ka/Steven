b2f761012aa808dba7e7e3bab48abe2d6d7bc755
Trup10ka <lukys3036@gmail.com>
2024-05-12 23:41:55 +0200

    Fix attachHeader paramether method - not attaching '=' sign

--- shared/src/commonMain/kotlin/me/trup10ka/shared/util/HttpUtil.kt
+++ shared/src/commonMain/kotlin/me/trup10ka/shared/util/HttpUtil.kt
@@ -3,8 +3,8 @@ package me.trup10ka.shared.util
 fun String.attachHeaderParam(paramName: String, param: String, attachAmpersand: Boolean = true): String
 {
     if (this.contains("?"))
-        return "$this${if (attachAmpersand) "&" else ""}$paramName$param"
-    return "$this?$paramName$param"
+        return "$this${if (attachAmpersand) "&" else ""}$paramName=$param"
+    return "$this?$paramName=$param"
 }
 
 infix fun String.withLocation(location: String): String
