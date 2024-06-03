0da61231161645e2523f42cf57b0f51a48fd05f9
Trup10ka <lukys3036@gmail.com>
2024-06-03 11:58:56 +0200

    Add KDocs

--- webapp/src/jsMain/resources/scripts/geolocation.js
+++ webapp/src/jsMain/resources/scripts/geolocation.js
@@ -1,3 +1,9 @@
+/**
+ * JS function to get the current location of the user
+ *
+ * Is called from Kotlin code in {@link JSPureGeoProvider}
+ * @param processLocation
+ */
 function callJsGetLocation(processLocation)
 {
     navigator.geolocation.getCurrentPosition(
